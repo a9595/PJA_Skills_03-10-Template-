38f898d06bf64e6f8f78dd5648bd2b03c04729ce
Andrii Kovalchuk <tieorange@gmail.com>
2015-12-06 13:01:56 +0100

    init

---
+++ PJA_Skills_032/Shell.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using PJA_Skills_032.Pages;
+using PJA_Skills_032.Presentation;
+
+namespace PJA_Skills_032
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            var vm = new ShellViewModel();
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Welcome", PageType = typeof(WelcomePage) });
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 1", PageType = typeof(Page1) });
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 2", PageType = typeof(Page2) });
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 3", PageType = typeof(Page3) });
+            vm.MenuItems.Add(new MenuItem { Icon = "I", Title = "Page777", PageType = typeof(Page777)});
+
+            // select the first menu item
+            vm.SelectedMenuItem = vm.MenuItems.First();
+
+            this.ViewModel = vm;
+        }
+
+        public ShellViewModel ViewModel { get; private set; }
+
+        public Frame RootFrame
+        {
+            get
+            {
+                return this.Frame;
+            }
+        }
+    }
+}
